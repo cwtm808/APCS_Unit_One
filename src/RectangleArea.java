e38bb3852e2046e2372ed9400f1dc3cdd61f682d
tonymeng <mengji@ssfs.org>
2021-09-09 11:00:23 -0400

    Unit 1.3

--- src/RectangleArea.java
+++ src/RectangleArea.java
@@ -1,13 +1,17 @@
+import java.awt.geom.Area;
+
 public class RectangleArea {
 
+
+    private static Object Area;
 
     public static void main(String[] args) {
         int length = 14;
         //declare and initialize width
-
+        int width = 6 ;
         //declare area - use length and width to calculate area
-
+        int Area= 84;
         // output the answer
-
+        System.out.println("Area = 84");
     }
 }
